b1a0c8ca6767d78f171bca8124ecb30f50a071ee
ivan <ivan.proekt00@mail.ru>
2023-10-22 23:44:20 +0500

    Task 3

---
+++ Tyuiu.KlokovIA.Sprint2.Task3.V20/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KlokovIA.Sprint2.Task3.V20.Lib;
+
+namespace Tyuiu.KlokovIA.Sprint2.Task3.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Клоков И. А. | ИСПБ-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Логические операции                                               *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Клоков Иван Анатальевич | ИСПБ-23-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y      *");
+            Console.WriteLine("* с использованием вложенных оператор if-else, где пользователь вводит    *");
+            Console.WriteLine("* значение переменной X с клавиатуры. Округлить полученное значение до    *");
+            Console.WriteLine("* трех знаков после запятой;                                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x;
+
+            Console.WriteLine("Введите значение переменной X:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                        *");
+            Console.WriteLine("*************************************************************************************");
+
+            double y = Math.Round(ds.Calculate(x), 3);
+
+            Console.WriteLine(y);
+            Console.ReadKey();
+        }
+    }
+}
